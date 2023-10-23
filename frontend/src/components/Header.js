75b38eb0a947f9149ec6df6dd81bda1423f6e4a4
kw19657 <106737706+kw19657@users.noreply.github.com>
2023-10-23 12:54:34 +0800

    added react-router-bootsrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/logo.png'
 
 
 const Header = () => {
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Navbar bg="dark" variant="dark" expand="hd" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/"><img src={logo} alt="proshop"/>ProShop</Navbar.Brand>
+                <LinkContainer to='/'>
+                <Navbar.Brand>
+                    <img src={logo} alt="proshop"/>
+                    ProShop
+                </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart"><FaShoppingCart /> Cart </Nav.Link>
+                        <LinkContainer to='/cart'>
+                        <Nav.Link>
+                            <FaShoppingCart /> Cart 
+                        </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                        <Nav.Link>
+                            <FaUser /> Sign In
+                        </Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Card, CardBody } from "react-bootstrap";
+import { Card, CardBody } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant="top" />
-        </a>
+        </Link>
 
         <CardBody>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as="div">
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
 
             <Card.Text as="div">
                 ${product.price}
