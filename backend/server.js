8d5f1457ebf851f9c24005ab1f38a2c5cfb7aff3
kw19657 <106737706+kw19657@users.noreply.github.com>
2023-10-24 09:44:25 +0800

    insatlled redux

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5000;
 
-connectDB(); // connect to MongoDB
+connectDB(); // connect to database
 
 const app = express();
 
@@ -13,13 +14,9 @@ app.get('/', (req, res) => {
     res.send('yass  API is running..');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server running on port ${port}`))
