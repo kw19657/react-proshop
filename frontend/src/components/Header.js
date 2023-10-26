ac5daf3ead1e50fdaa299e80302614263e28ed41
kw19657 <106737706+kw19657@users.noreply.github.com>
2023-10-26 15:25:27 +0800

    added Admin screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
-import { useSelector } from 'react-redux';
-import logo from '../assets/logo.png'
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
+import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
     const { cartItems } = useSelector((state) => state.cart);
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="hd" collapseOnSelect>
+        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
                 <LinkContainer to='/'>
                 <Navbar.Brand>
@@ -34,11 +53,33 @@ const Header = () => {
                             }
                         </Nav.Link>
                         </LinkContainer>
-                        <LinkContainer to='/login'>
-                        <Nav.Link>
+                        { userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item>Profile</NavDropdown.Item>    
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>
+                                    Logout
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        ) : ( <LinkContainer to='/login'>
+                        <Nav.Link href='/login'>
                             <FaUser /> Sign In
                         </Nav.Link>
-                        </LinkContainer>
+                        </LinkContainer>) }
+                       {userInfo && userInfo.isAdmin && (
+                        <NavDropdown title='Admin' id='adminmenu'>
+                            <LinkContainer to='/admin/productlist'>
+                                <NavDropdown.Item>Products</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to='/admin/userlist'>
+                                <NavDropdown.Item>Users</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to='/admin/orderlist'>
+                                <NavDropdown.Item>Orders</NavDropdown.Item>
+                            </LinkContainer>
+                        </NavDropdown>
+                       )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
